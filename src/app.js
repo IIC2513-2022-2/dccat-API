1391586695d8c2cd01370ad3b3c0c8e7b76aa072
Benjamín Lillo <benjalso99@gmail.com>
2022-09-04 12:43:45 -0400

    feat(koa):add-empty-router

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import Koa from "koa";
 import koaBody from "koa-body";
 import KoaLogger from "koa-logger";
+import router from "./routes.js";
 
 const PORT = 3000;
 
@@ -15,6 +16,8 @@ app.use(koaBody());
 app.use((ctx, next) => {
   ctx.body = "Hello 2048 DCC!";
 })
+
+app.use(router.routes());
 
 app.listen(PORT, () => {
   console.log(`Starting app in port ${PORT}`);

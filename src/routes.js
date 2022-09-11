92125440ee753218cd42b7017ffa68a4ee778cb3
domingo2000 <domingo.edwards@uc.cl>
2022-09-11 16:21:55 -0300

    Refactor/deleted-dummy-get-endpoint

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,8 @@
 const Router = require('koa-router');
 const movies = require('./routes/movies');
-const firstGet = require('./routes/firstGet');
 
 const router = new Router();
 
 router.use('/movies', movies.routes());
-router.use('/firstGet', firstGet.routes());
 
 module.exports = router;

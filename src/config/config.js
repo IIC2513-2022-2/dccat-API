27bc6ab5749c995dcb77dbeda3e4e4950f6536fe
domingo2000 <domingo.edwards@uc.cl>
2022-09-12 19:46:26 -0300

    Fix/sequelize-config-location

---
+++ src/config/config.js
@@ -0,0 +1,26 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = {
+  "development": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": `${process.env.DB_NAME}_development`,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": `${process.env.DB_NAME}_test`,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": `${process.env.DB_NAME}_production`,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres"
+  }
+}

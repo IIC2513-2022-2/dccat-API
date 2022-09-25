5340ca272c8ac0d38a555697befef60d5087dfd2
IgnaciaJ <ignacia.joustra@uc.cl>
2022-09-25 18:49:16 -0300

    feat: get match endpoint with specific id

--- src/routes/matches.js
+++ src/routes/matches.js
@@ -2,9 +2,10 @@ const Router = require('koa-router');
 
 const router = new Router();
 
-router.get('matches.show', '/', async (ctx) => {
+router.get('matches.show', '/:id', async (ctx) => {
   try {
-    const match = await ctx.orm.Match.findOne(
+    const match = await ctx.orm.Match.findByPk(
+      ctx.params.id,
       {
         include: [
           { model: ctx.orm.Player, as: 'player1' },
@@ -13,20 +14,32 @@ router.get('matches.show', '/', async (ctx) => {
         ],
       },
     );
+    if (!match) {
+      ctx.throw(404);
+    }
+    const requestPlayer = await ctx.orm.Player.findByPk(ctx.request.query.id);
     const player1 = await match.getPlayer1();
     const player2 = await match.getPlayer2();
-    const currentPlayer = await match.getCurrentPlayer();
     const plays = await match.getPlays();
+    if (![player1.id, player2.id].includes(requestPlayer.id)) {
+      ctx.throw('No tienes permiso para acceder al tablero', 403);
+    }
+    const moves0 = plays.filter((play) => play.player === requestPlayer.id)
+      .map((play) => [play.dataValues.x, play.dataValues.y], []);
+    const moves1 = plays.filter((play) => play.player !== requestPlayer.id)
+      .map((play) => [play.dataValues.x, play.dataValues.y], []);
+
     ctx.body = {
-      match,
+      0: moves0,
+      1: moves1,
+      current: match.current,
+      turno: match.turno,
       player1,
       player2,
-      currentPlayer,
-      plays,
     };
   } catch (error) {
     console.log(error);
-    ctx.throw(404);
+    ctx.throw(error);
   }
 });
 

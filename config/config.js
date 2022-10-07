3246d84d32a6d202d8b7f307ac2896c28c3927fb
sebacarrasco <sebacarrasco@uc.cl>
2022-10-07 20:49:36 -0300

    fix: delete package-lock.json and add db_url to production config

--- config/config.js
+++ config/config.js
@@ -17,10 +17,6 @@ module.exports = {
     dialect: 'postgres',
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: `${process.env.DB_NAME}_production`,
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
+    use_env_variable: 'DATABASE_URL',
   },
 };

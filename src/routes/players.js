402e1df852e0d8691bc7d2cc8889c6b6a49951f1
Diego Rosselot <drosselot@uc.cl>
2022-10-11 20:05:34 -0300

    feat: add player post and password hash

--- src/routes/players.js
+++ src/routes/players.js
@@ -1,6 +1,22 @@
 const Router = require('koa-router');
+const bcrypt = require('bcrypt');
+
 
 const router = new Router();
+
+router.post('players.create', '/', async (ctx) => {
+  try {
+    const hash_contraseña = await bcrypt.hash(ctx.request.body.password, 5);
+    const player = await ctx.orm.Player.create({
+      nickname: ctx.request.body.nickname,
+      email: ctx.request.body.email,
+      hash_contrasena: hash_contraseña
+    });
+    ctx.status = 201;
+  } catch (error) {
+    ctx.throw(error);
+  }
+})
 
 router.get('players.show', '/', async (ctx) => {
   try {

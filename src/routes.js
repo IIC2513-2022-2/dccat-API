2fdb9b1a64a48f05a47696d1d8df514aee8da833
sebacarrasco <sebacarrasco@uc.cl>
2022-11-16 21:48:25 -0300

    tests: test setup

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
+const dotenv = require('dotenv');
 const Router = require('koa-router');
 const auth_middle = require('./middlewares/auth');
 const matches = require('./routes/matches');
 const players = require('./routes/players');
 const plays = require('./routes/plays');
 const auth = require('./routes/auth');
-const protected = require('./routes/protected');
+const _protected = require('./routes/protected');
 const jwt = require('koa-jwt');
+
+dotenv.config();
 
 
 const router = new Router();
@@ -18,6 +21,6 @@ router.use('/plays', auth_middle, plays.routes());
 
 router.use(jwt({ secret: process.env.JWT_SECRET, key: 'tokendata' }));
 
-router.use(protected.routes());
+router.use(_protected.routes());
 
 module.exports = router;
